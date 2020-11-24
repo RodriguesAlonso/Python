7c98760b3c4a80f7fc2f83d79dcf45e61d12b78c
João Alonso <joaoalonso42@gmail.com>
2020-11-24 10:00:43 -0300

    mais testse

--- gitTest.py
+++ gitTest.py
@@ -2,3 +2,10 @@ print('Hellow gitHub')
 print('Nice job')
 #one more test remote
 #test four
+print('teste')
+a = 10
+a = eval(input("digite um número:"))
+if a == 0:
+    print(f'legal{a}')
+else:
+    print(f'legal{a}')
