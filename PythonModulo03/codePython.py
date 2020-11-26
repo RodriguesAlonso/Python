f150d79075b96212ff3d5087ef976e153caf5017
João Alonso <joaoalonso42@gmail.com>
2020-11-25 21:05:11 -0300

    resolução exercícios

--- PythonModulo03/codePython.py
+++ PythonModulo03/codePython.py
@@ -1,1 +1,16 @@
 print('algo')
+
+##
+def par(x):  # função identifica par ou impar
+    if x % 2 == 0:
+        print(f'{x} é par')
+    else:
+        print(f'{x} é impar')
+
+
+def lisA(tamanho, alcance):  # Cria listas aleatórias
+    from random import randint
+    lista = []
+    for x in range(tamanho):
+        lista.append(randint(0, alcance))
+    return lista
