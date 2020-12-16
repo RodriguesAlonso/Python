303d3aefd973e7cb259c43ebf5721863c14b0eec
João Alonso <joaoalonso42@gmail.com>
2020-12-16 17:09:08 -0300

    study virtual envronment

--- PythonModulo03/teste.py
+++ PythonModulo03/teste.py
@@ -1,11 +1,10 @@
-x = 30 / 5 + 4 * 10 - 15 == 31
-y = 10 - 5 + 14 * 10 / 15 == 158
-w = 30 - 5 - 14 + 10 / 15 == 122
-k = 30 + 5 - 14 * 10 / 15 == 162
-lista = [x, y, w, k]
-for a in lista:
-    if True:
-        print(f"{a} é verdadeiro")
-    print(f'{a} é falso')
+from flask import Flask, app
 
-print("Hello, git Hub")    
+app = Flask(__name__)
+
+@app.route("/")
+def index():    
+    return "Hello, World!"
+    
+if __name__ == "__main__":
+    app.run()
