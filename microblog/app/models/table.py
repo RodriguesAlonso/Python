23fe32ddd7df50bfa05cd8cccc4f0345668bad9f
João Alonso <joaoalonso42@gmail.com>
2020-12-23 19:17:12 -0300

    tables

--- microblog/app/models/table.py
+++ microblog/app/models/table.py
@@ -1,34 +1,48 @@
-from sys import int_info
+from enum import unique
+from sqlalchemy.sql.schema import ForeignKey
 from app import db
 
 
 class User(db.Model):
-    __tablename__ = 'users'
-    id
-    username
-    password
-    email
+    __tablename__ = "users"
 
-    def __init__(self) -> None:
-        super().__init__()
+    id = db.Collumn(db.Integer, primary_key=True)
+    username = db.Collumn(db.String, unique=True)
+    password = db.Collumn(db.String)
+    email = db.Collumn(db.String, unique=True)
 
-    def __repr__(self) -> str:
-        return super().__repr__()
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class Post(db.Model):
-    __tablename__ = 'posts'
-    id
-    content
-    user_if
+    __tablename__ = "posts"
 
-    def __init__(self) -> None:
-        super().__init__()
+    id = db.Collumn(db.Integer, primary_key=True)
+    content = db.Collumn(db.Text)
+    user_id = db.Collumn(db.Integer, db.ForeingKey('user.id'))
 
-    def __repr__(self) -> str:
-        return super().__repr__()
+    user = db.relationship('Post', foreing_key=user_id)
 
-class Follower(db.Model):
+    def __init__(self, content, user_id):
+        self.content = content
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+
+class Follow(db.Model):
     __tablename__ = 'follow'
-    id
-    user_id
-    follower_id
+
+    id = db.Collumn(db.Integer, primary_key=True)
+    user_id = db.Collumn(db.integer, db.ForeingKeys('user.id'))
+    follower_id = db.Collumn(db.Integer, db.ForeingKeys('users.id'))
+
+    user = db.relationship('User', foreing_keys=user_id)
+    follower = db.relationship('User', foreing_keys=follower_id)
